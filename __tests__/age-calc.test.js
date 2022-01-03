278f15be6a60974de1d158169a1a76286a5c4ab3
bclauson <clauson.branden@gmail.com>
2022-01-02 19:41:36 -0800

    add first test

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -4,7 +4,11 @@ describe('Space Age', () => {
   let reusableSpace;
 
   beforeEach(() => {
-    reusableSpace = new SpaceAge();
+    reusableSpace = new SpaceAge(22,75);
   })
+
+  test('Should construct a SpaceAge object with constructor', () => {
+    console.log(reusableSpace);
+  });
 
 });
